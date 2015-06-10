40160abefd7a8c402546dd69232039e6c8365cea
Jeongbin Kim <dev@menumap.co.kr>
2015-06-10 15:45:15 +0900

    Add flash layouts on the top and notice#create

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -3,4 +3,33 @@ class NoticesController < ApplicationController
   def index
 		@notices = Notice.all
   end
+
+	def new
+		@notice = Notice.new
+	end
+
+	def create
+		new_notice = Notice.new(notice_params)
+		if new_notice.save
+			flash[:alert] = "success new notice"
+			redirect_to qna_url
+		else
+			flash[:alert] = "fail new notice"
+			redirect_to qna_url
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+		def notice_params
+			params.require(:notice).permit(:question, :answer)
+		end
 end
