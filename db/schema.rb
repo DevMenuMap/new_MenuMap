376a49745b6166251afc2c6669fad5886b767e25
Jeongbin Kim <dev@menumap.co.kr>
2015-06-11 14:59:28 +0900

    Add category, subcategory and those relationship models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610075709) do
+ActiveRecord::Schema.define(version: 20150611055744) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "category_relationships", force: :cascade do |t|
+    t.integer  "category_id",    limit: 4
+    t.integer  "subcategory_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "category_relationships", ["category_id"], name: "index_category_relationships_on_category_id", using: :btree
+  add_index "category_relationships", ["subcategory_id"], name: "index_category_relationships_on_subcategory_id", using: :btree
 
   create_table "notices", force: :cascade do |t|
     t.string   "question",   limit: 255
@@ -28,4 +44,12 @@ ActiveRecord::Schema.define(version: 20150610075709) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "subcategories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_foreign_key "category_relationships", "categories"
+  add_foreign_key "category_relationships", "subcategories"
 end
