ea2139474406b9d96fab9b5a5558ceb33586bd1f
Jeongbin Kim <dev@menumap.co.kr>
2015-06-11 13:57:27 +0900

    Locate questions#new on the Qna page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 	get "search"	=> "home#search"
 
 	resources :notices, except: [:index, :show]
-	resources :questions
+	resources :questions, except: [:new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
