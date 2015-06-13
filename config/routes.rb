509534e5baebd096701e37f0677606621f1a5d2e
Jeongbin Kim <dev@menumap.co.kr>
2015-06-13 11:03:38 +0900

    Install Devise gem and delete unnecessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -11,18 +11,6 @@ Rails.application.routes.draw do
 	resources :notices, except: [:index, :show]
 	resources :questions, except: [:new]
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
   # Example resource route with options:
   #   resources :products do
   #     member do
