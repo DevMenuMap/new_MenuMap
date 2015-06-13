5b86f9bd2e795671f9bc11ad9f63b109beb12e60
Jeongbin Kim <dev@menumap.co.kr>
2015-06-13 11:18:30 +0900

    Generate devise users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,14 @@ Rails.application.routes.draw do
 	resources :questions, except: [:new]
 
 	# User & Admin with devise gem.
-  devise_for :users
+  devise_for :users, controllers: {
+		# confirmations: "users/confirmations",
+		passwords: "users/passwords",
+		registrations: "users/registrations",
+		sessions: "users/sessions",
+		# unlocks: "users/unlocks",
+		# omniauth_callbacks: "users/omniauth_callbacks"
+	}
 
   # Example resource route with options:
   #   resources :products do
