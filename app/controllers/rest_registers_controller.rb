ae41d66fab4ea24dd75dcc1eb2172f82833f1c59
Jeongbin Kim <dev@menumap.co.kr>
2015-06-15 21:52:42 +0900

    Add CRUD for RestRegister

--- app/controllers/rest_registers_controller.rb
+++ app/controllers/rest_registers_controller.rb
@@ -1,8 +1,10 @@
 class RestRegistersController < ApplicationController
   def index
+		@rest_registers = RestRegister.all
   end
 
   def show
+		@rest_register = RestRegister.find(params[:id])
   end
 
   def new
@@ -12,8 +14,27 @@ class RestRegistersController < ApplicationController
   end
 
 	def create
+		@rest_register = RestRegister.new(register_params)	
+		if @rest_register.save
+			flash[:alert] = "succeed in rest_registers#create"
+			redirect_to rest_registers_path
+		else
+			flash[:alert] = "fail in rest_registers#create"
+			redirect_to new_rest_register_path
+		end
 	end
 
 	def destroy
+		@rest_register = RestRegister.find(params[:id]).destroy
+		flash[:alert] = "succeed in rest_registers#destroy"
+		redirect_to :back
 	end
+
+	private
+		def register_params
+			params.require(:rest_register).permit(:user_id, :email, :name,
+																						:category_id, :subcategory_id,
+																						:addr, :phnum, :delivery,
+																						:open_at, :etc)
+		end
 end
