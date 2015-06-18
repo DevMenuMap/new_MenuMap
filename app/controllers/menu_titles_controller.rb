5594b78a7cd5126290ffc8db5add9bccf53d1862
Jeongbin Kim <dev@menumap.co.kr>
2015-06-18 13:06:35 +0900

    Make MenuTitle controller

---
+++ app/controllers/menu_titles_controller.rb
@@ -0,0 +1,13 @@
+class MenuTitlesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ app/helpers/menu_titles_helper.rb
@@ -0,0 +1,2 @@
+module MenuTitlesHelper
+end
