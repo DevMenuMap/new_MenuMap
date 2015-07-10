15794020eeb8d40e1e64acd8f80ffdf90ca250c6
unknown <luke9407@snu.ac.kr>
2015-07-10 17:54:29 +0900

    Modify comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,37 +5,51 @@ class CommentsController < ApplicationController
 
   def create
 		@comment = Comment.new(comment_params)
+
 		if @comment.save
 			flash[:alert] = "Succeed comment#create"
 		else
 			flash[:alert] = "Fail comment#create"
 		end
-		redirect_to restaurant_url(@comment.restaurant)
+
+		redirect_to_restaurant_page
 	end
 
   def edit
   	@comment = Comment.find(params[:id])
+  	redirect_to_restaurant_page
   end
 
   def update
 		@comment = Comment.find(params[:id])
+
 		if @comment.update(comment_params)
 			flash[:alert] = "Succeed comment#update"
 		else
 			flash[:alert] = "Fail comment#update"
 		end
-		redirect_to restaurant_url(@comment.restaurant)
+
+		redirect_to_restaurant_page
 	end
 
   def destroy
 		@comment = Comment.find(params[:id])
 		@comment.update(active: false)
 		flash[:alert] = "Succeed comment#destroy"
-		redirect_to restaurant_url(@comment.restaurant)
+		redirect_to_restaurant_page
 	end
 
 	private
 		def comment_params
 			params.require(:comment).permit(:restaurant_id, :contents)
+		end
+
+		def redirect_to_restaurant_page
+			@restaurant = @comment.restaurant
+
+			respond_to do |format|
+				format.html	{ redirect_to restaurant_url(@restaurant) }
+				format.js		{ render layout: false }
+			end
 		end
 end
