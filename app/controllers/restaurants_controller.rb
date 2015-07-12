a670b9a8ccbc53ce15797b227e1426ebdbdc9763
Jeongbin Kim <dev@menumap.co.kr>
2015-07-13 00:16:12 +0900

    Search on delivery

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,7 @@ class RestaurantsController < ApplicationController
 	# to check if subcategory is not "all thing"; to specify subcategory
 
   def index
-		@restaurants = Restaurant.paginate(:page => params[:page], :per_page => 10)
+		@restaurants = Restaurant.search(params[:delivery]).paginate(page: params[:page], per_page: 10)
   end
 
   def show
