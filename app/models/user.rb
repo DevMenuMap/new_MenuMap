3eea03bcfc21296da89c1abaca24f4e8e44a826a
unknown <luke9407@snu.ac.kr>
2015-07-16 22:54:53 +0900

    Add username validation and show username in header

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,55 @@ class User < ActiveRecord::Base
 	has_many :rest_errs
 	has_many :menus
 	has_many :comments
+
+
+	### Validations
+	validates :username, presence: true, uniqueness: { case_sensitive: false },
+											 format: {
+											 	with: /\A[a-zA-Z_\p{Hangul}][a-zA-Z0-9_\p{Hangul}]+\z/,
+											 	message: "invalid username"
+											 }, length: { maximum: 20 }
+
+	# Custom validators
+	validate :no_slang
+	validate :uniqueness_in_mysql
+	validate :no_particular_word
+	validate :size_in_byte
+
+
+	### Instance method
+	def no_slang
+    if Slang.where("? LIKE CONCAT('%', name, '%')", username).present?
+      errors.add(:username, "Slang")
+    end
+  end
+
+  def uniqueness_in_mysql
+  	if User.where(username: username).present?
+  		errors.add(:username, "Duplicate")
+  	end
+  end
+
+  def no_particular_word
+  	impossibles = ["메뉴맵", "menumap", "운영자", "profile", "store",
+  								 "restaurant", "login", "logout", "signup", "signin",
+  									"signout", "session", "user", "username", "test",
+  									"help", "index", "home", "about", "contact", "root",
+  									"manual", "delete", "destroy", "create", "make",
+  									"show", "new", "post", "comment", "page", "invalid",
+  									"valid"]
+  	username_downcase = username.downcase
+  	if username_downcase.end_with? "s"
+  		username_downcase = username_downcase[0...-1]
+  	end
+  	if impossibles.include? username_downcase
+  		errors.add(:username, "Impossible")
+  	end
+  end
+
+  def size_in_byte
+  	if username.bytesize < 6
+  		errors.add(:username, "Too short")
+  	end
+  end
 end
