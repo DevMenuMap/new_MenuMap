ffa5d450e43a7a41419ac1f8545b254d518043ab
Jeongbin Kim <dev@menumap.co.kr>
2015-07-18 20:26:01 +0900

    Create mymap when posting mymaps#new form

--- app/controllers/mymaps_controller.rb
+++ app/controllers/mymaps_controller.rb
@@ -18,4 +18,21 @@ class MymapsController < ApplicationController
 			format.js { render layout: false }
 		end
 	end
+
+	def create
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@user = current_user
+		@mymap = @restaurant.mymaps.new(mymap_params)
+		@mymap.user_id = @user.id
+		@mymap.save
+
+		respond_to do |format|
+			format.js { render layout: false }
+		end
+	end
+
+	private
+		def mymap_params
+			params.require(:mymap).permit(:rating, :group, :contents)
+		end
 end
