3deedf90cfcd6d55449171a98b43af52b1d21ef0
nohyonggoo <yong115_@naver.com>
2015-07-27 16:35:49 +0900

    Succeed in jQuery on basic submit

--- app/assets/javascripts/questions.coffee
+++
@@ -1,5 +0,0 @@
-jQuery ->
-	$("#new_question").bind "submit", (event) ->
-		if !$("#new_question #question_contents").val()
-			alert("경고: contents should not be blank!")
-			false
---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,8 @@
+function formValidation(e) {
+	alert("test alert");
+	e.preventDefault();
+//		if ( $.trim( $("#quenstion_contents").val() ) == "" ) {
+//			alert("경고: contents should not be blank!");
+//			return false;
+//		}
+};
