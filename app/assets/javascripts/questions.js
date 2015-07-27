5250c606d21620c39157affcedb22deeb8dffe6b
nohyonggoo <yong115_@naver.com>
2015-07-27 22:34:28 +0900

    Make questions.js for question_new page

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,8 +1,13 @@
-function formValidation(e) {
-	alert("test alert");
-	e.preventDefault();
-//		if ( $.trim( $("#quenstion_contents").val() ) == "" ) {
-//			alert("경고: contents should not be blank!");
-//			return false;
-//		}
+function formValidation(e, id) {
+	var allInputs = $(id).children("input:text, textarea");
+	var i;
+	var l = allInputs.length;
+
+	for(i = 0; i < l; i++) {
+		if ($.trim( allInputs.eq(i).val() ) == "" ) {
+			alert("경고: " + allInputs.eq(i).attr("data-validate") + " should not be blank!");
+			e.preventDefault();
+			break;
+		}
+	}
 };
