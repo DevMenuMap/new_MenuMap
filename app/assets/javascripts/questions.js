0b4bddd600a8626b26dfbed442a68d38c42bbb52
nohyonggoo <yong115_@naver.com>
2015-07-27 23:38:48 +0900

    Add data-rule and validate()

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,13 +1,13 @@
-function formValidation(e, id) {
-	var allInputs = $(id).children("input:text, textarea");
-	var i;
-	var l = allInputs.length;
-
-	for(i = 0; i < l; i++) {
-		if ($.trim( allInputs.eq(i).val() ) == "" ) {
-			alert("경고: " + allInputs.eq(i).attr("data-validate") + " should not be blank!");
-			e.preventDefault();
-			break;
-		}
-	}
-};
+//function formValidation(e, id) {
+//	var allInputs = $(id).children("input:text, textarea");
+//	var i;
+//	var l = allInputs.length;
+//
+//	for(i = 0; i < l; i++) {
+//		if ($.trim( allInputs.eq(i).val() ) == "" ) {
+//			alert("경고: " + allInputs.eq(i).attr("data-validate") + " should not be blank!");
+//			e.preventDefault();
+//			break;
+//		}
+//	}
+//};
