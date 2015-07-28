97d1df8b03d3943cacb3a00f549ac0c87156c9d4
nohyonggoo <yong115_@naver.com>
2015-07-28 16:57:07 +0900

    Apply upload plugin to rest_register page

--- app/assets/javascripts/form_validations.js
+++ app/assets/javascripts/form_validations.js
@@ -1,31 +1,9 @@
-//# Check if image file's size is over 15 megabytes
-//#sizeChecker = (file_size) ->
-//#	if file_size > 15
-//#		alert("so big image")
-//
-//# Check if the file has image filetype
-//#fileTypeChecker = (file_name) ->
-//#	file_type = file_name.split(".").pop().toLowerCase()
-//	# number '0' has special meaning
-//#	if file_type && $.inArray(file_type, ["jpg", "jpeg", "png", "gif"]) == -1
-//#		alert("wrong file_type")
-//#		false
-//
-//#jQuery ->
-//#	$("[data-validate='image']").change ->
-//#		file_size = this.files[0].size/1024/1024
-//#		file_name = this.files[0].name
-//#		sizeChecker(file_size)
-//#		fileTypeChecker(file_name)
-
-
 //variable to check file
 var valid = true;
 
 function sizeChecker(file_size) {
 	file_size = file_size / 1024 / 1024;
 	if (file_size > 15) {
-		alert("so big image");
 		return false;
 	} else { return true; }
 };
@@ -33,7 +11,6 @@ function sizeChecker(file_size) {
 function fileTypeChecker(file_name) {
 	var ext = file_name.split(".").pop().toLowerCase();
 	if ($.inArray( ext, ["jpg", "jpeg", "png", "gif"] ) == -1 ) {
-		alert("wrong file type"); 
 		return false;
 	} else { return true; }
 };
@@ -49,7 +26,7 @@ function formValidation() {
 function checkValidation(e) {
 	if (valid) {
 	} else {
-		alert("fail");
+		alert("error occured in formValidation()");
 		e.preventDefault();
 	}
 };
