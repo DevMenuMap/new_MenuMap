d43902186ba5f1c6fd8eb3f77d8df35da708a7c1
nohyonggoo <yong115_@naver.com>
2015-08-11 01:01:38 +0900

    Replace home.coffee with home.js

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,34 @@
+$(document).ready( function() {
+	$("#category_select").change( function() {
+		$.ajax({
+			url: window.location.origin + '/home/update_subcategories',
+			dataType: "script",
+			data: {
+				category: $("#category_select").val()
+			}
+		});
+	});
+	// variable for autocompleting address name on search page
+	var addrComplete = $("#addrcomplete").autocomplete( {
+		// autoFocus: false
+		source: window.location.origin + '/home/addrcomplete',
+		minLength: 0,
+		focus: function(event, ui) {
+			$("#addrcomplete").val(ui.item.name);
+			return false;
+		},
+		select:	function(event, ui) {
+			$("#addrcomplete").val(ui.item.name);
+			return false;
+		}
+	}).data('uiAutocomplete');
+
+	// Only when there is #addrcomplete(search page), this code works
+	if (addrComplete) {
+		addrComplete._renderItem = function(ul,item) {
+			return $("<li></li>").data("item.autocomplete", item)
+											.append("<a>" + item.name + "</a>")
+											.appendTo(ul);
+		};
+	};
+});
