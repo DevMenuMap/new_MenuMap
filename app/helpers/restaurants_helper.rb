6f63cceeca50febfc3db415fe733ca1f2fc85d6d
Jeongbin Kim <dev@menumap.co.kr>
2015-08-23 21:53:59 +0900

    Complete basic design of index page

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -9,4 +9,51 @@ module RestaurantsHelper
 			link_to "완전등록", restaurant_path(restaurant, anchor: "menu_section_top")
 		end
 	end
+
+	# Restaurant's name with left border which has color for menu_on.
+	def restaurant_name_with_menu_on(restaurant)
+		color = menu_color(restaurant)
+		content_tag :span, class: "name", style: "border-left: 4px solid #{color}" do
+			" " + restaurant.name
+		end
+	end
+
+	def menu_color(restaurant)
+		case restaurant.menu_on
+		when 1 
+			"orange"
+		when 0
+			"gray"
+		else
+			"green"
+		end
+	end
+
+	# Puts 2 title menus or subcategory name.
+	def menus_or_category(restaurant)
+		if restaurant.menus.blank?
+			content_tag :span, restaurant.subcategory.name
+		else
+			content_tag :span, restaurant.title_menus(2).join(", ")
+		end
+	end
+
+	# Puts string when delivery is possible.
+	def delivery_possible?(restaurant)
+		if restaurant.delivery
+			content_tag :span, "배달", class: "label label-info"
+		end
+	end
+
+	def gu_and_dong(restaurant)
+		restaurant.addr.split[1..2].join(" ")
+	end
+
+	def restaurant_picture(restaurant)
+		if picture = restaurant.pictures.first 
+			image_tag picture.img.url, alt: "#{restaurant.name} 사진", class: "thumbnail rst_img"
+		else 
+			image_tag "restaurants/default_restaurant.png", alt: "현재 등록된 사진이 없습니다.", class: "thumbnail rst_img"
+		end 
+	end
 end
