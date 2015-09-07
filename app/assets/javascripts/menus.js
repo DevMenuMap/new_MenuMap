f0b3e18d662a1c7f83133852eac1a5ca1a2ebecc
Jeongbin Kim <dev@menumap.co.kr>
2015-09-07 14:03:21 +0900

    Alert to select menu_title when creating a new menu

--- app/assets/javascripts/menus.js
+++ app/assets/javascripts/menus.js
@@ -1,3 +1,17 @@
+// Show or make invisible when clicking menu_titles
+function expandTitleForm(selectedTitle) {
+	if (selectedTitle.val() == 'new') {
+		$('#menu_title_form').css('display', 'block');
+		selectedTitle.closest('form').find('label[for=menu_name]').css('margin-top', '15px');
+	} else {
+		if (selectedTitle.val() == 'none') {
+			alert('메뉴목록을 선택해주세요.');
+		};
+		$('#menu_title_form').css('display', 'none');
+		selectedTitle.closest('form').find('label[for=menu_name]').css('margin-top', '0px');
+	};
+};
+
 $(document).on('ready page:load', function() {
 	$('#new_menu').validate({
 		rules: {
@@ -19,17 +33,6 @@ $(document).on('ready page:load', function() {
 	// Change chevron on cascading dropdown.
 	$('#new_menu_section > a > div').click(function() {
 		$(this).find("i").toggleClass("fa-chevron-up");
-	});
-
-	// Show or make invisible when clicking menu_titles
-	$('#menu_menu_title_id').change(function(){
-		if ($(this).val() == 'new') {
-			$('#menu_title_form').css('display', 'block');
-			$(this).closest('form').find('label[for=menu_name]').css('margin-top', '15px');
-		} else {
-			$('#menu_title_form').css('display', 'none');
-			$(this).closest('form').find('label[for=menu_name]').css('margin-top', '0px');
-		};
 	});
 
 	// Change menu list left border on click.
