f216d9f039ecc342e0e5f612cb5d5b1c096be6e2
Jeongbin Kim <dev@menumap.co.kr>
2015-09-09 18:48:56 +0900

    Change API keys

--- app/assets/javascripts/tracking_code_GA.js
+++ app/assets/javascripts/tracking_code_GA.js
@@ -3,4 +3,4 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-59102964-3', 'auto');
+ga('create', 'UA-59102964-1', 'auto');
