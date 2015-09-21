244fca54525632a5fef6ea568085bf0b7907144a
Jeongbin Kim <dev@menumap.co.kr>
2015-09-22 00:55:03 +0900

    Move menu autocomplete to js file

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -22,7 +22,20 @@ function commentContentsValidation(target) {
 	});
 };
 
+// Menu tags for comment
+function autocompleteMenus(targetName) {
+	var target = $(targetName);
+	target.tagit({
+		autocomplete: {
+			source: window.location.origin + '/restaurants/' + target.data('restaurant').toString() + '/menu_complete'
+		},
+		placeholderText: "#메뉴_태그"
+		// removeConfirmation: true,
+	});
+};
+
 $(document).on('ready page:load', function() {
 	disableCommentPaste('#comment_contents');
 	commentContentsValidation('#new_comment');
+	autocompleteMenus('.menu_comments_tag');
 });
