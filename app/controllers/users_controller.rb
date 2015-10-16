78fca6ceab9f7774280072a68142b6f06d1bcf12
Jeongbin Kim <dev@menumap.co.kr>
2015-10-16 15:55:28 +0900

    Users#deleted_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 	before_action :admin?
 
   def index
-		# @users = User.where("id > 10000000").order(id: :asc)
-		@users = User.order(id: :asc)
+		@users = User.where("id > 10000000").order(id: :asc)
+		@deleted_users = User.unscoped.where("active = 0").order(updated_at: :desc)
   end
 end
